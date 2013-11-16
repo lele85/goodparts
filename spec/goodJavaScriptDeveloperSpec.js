44cfb6ff2be84180c9f5d75db3ff58be1fad321b
Emanuele Rampichini <emanuele.rampichini@gmail.com>
2013-11-16 17:17:41 +0100

    More specs and repaired Function.prototype

--- spec/goodJavaScriptDeveloperSpec.js
+++ spec/goodJavaScriptDeveloperSpec.js
@@ -183,173 +183,302 @@ describe("A good JavaScript developer", function() {
         }
         expect(numberOfProperties).toBe(1);
     });
-    
-    it("should know that she can break to a labelled loop", function(){
+
+    it("should know that she can break to a labelled loop", function() {
         loop1:
-        while (true){
-            while(true){
-                while(true){
+                while (true) {
+            while (true) {
+                while (true) {
                     break loop1;
                 }
             }
         }
         expect(true).toBe(true);
     });
-    
-    it("should be aware that the compiler adds ; in unexpected ways", function(){
-       var hello = function(){
-           return //Compiler adds ; here
-           "hello world";
-       };
-       expect(typeof hello() === 'undefined').toBe(true);
+
+    it("should be aware that the compiler adds ; in unexpected ways", function() {
+        var hello = function() {
+            return //Compiler adds ; here
+            "hello world";
+        };
+        expect(typeof hello() === 'undefined').toBe(true);
     });
-    
-    it("should know operator precedences and use parenthesys to avoid confusion", function(){
+
+    it("should know operator precedences and use parenthesys to avoid confusion", function() {
         expect(1 + 2 * 3 - 12 + -2 * 4 * -1).toBe(3);
-        expect(false !== true && true ).toBe(true);
-        expect(true && false || true ).toBe(true);
+        expect(false !== true && true).toBe(true);
+        expect(true && false || true).toBe(true);
     });
-    
-    it("should know that possible types in javascript are 'number' 'string' 'boolean' 'object' 'function' and 'undefined' and some are wrong", function(){
-       expect(typeof []).toBe('object');
-       expect(typeof null).toBe('object');
-       expect(typeof 1).toBe('number');
-       expect(typeof Infinity).toBe('number');
-       expect(typeof NaN).toBe('number');
-       expect(typeof "").toBe('string');
-       expect(typeof false).toBe('boolean');
-       expect(typeof Object.prototype.hasOwnProperty).toBe("function");
-       expect(typeof undefined).toBe('undefined');
+
+    it("should know that possible types in javascript are 'number' 'string' 'boolean' 'object' 'function' and 'undefined' and some are wrong", function() {
+        expect(typeof []).toBe('object');
+        expect(typeof null).toBe('object');
+        expect(typeof 1).toBe('number');
+        expect(typeof Infinity).toBe('number');
+        expect(typeof NaN).toBe('number');
+        expect(typeof "").toBe('string');
+        expect(typeof false).toBe('boolean');
+        expect(typeof Object.prototype.hasOwnProperty).toBe("function");
+        expect(typeof undefined).toBe('undefined');
     });
-    
-    it("should know that a function literal can be anonymous or named, and you can use name for recursion", function(){
-        var exit =  false;
-        var hello = function salutation (){
-            if(exit){ return "hello world";}
+
+    it("should know that a function literal can be anonymous or named, and you can use name for recursion", function() {
+        var exit = false;
+        var hello = function salutation() {
+            if (exit) {
+                return "hello world";
+            }
             exit = true;
             return salutation();
-        };    
+        };
         expect(hello()).toBe("hello world");
     });
-    
-    it("should know that can use || to fill in default values", function(){
-       var salutation = {
-           italian : "ciao",
-           english : "hello"
-       };
-       
-       expect(salutation.italian).toBe("ciao");
-       expect(salutation["english"]).toBe("hello");
-       expect(salutation.spanish || "not supported").toBe("not supported");
+
+    it("should know that can use || to fill in default values", function() {
+        var salutation = {
+            italian: "ciao",
+            english: "hello"
+        };
+
+        expect(salutation.italian).toBe("ciao");
+        expect(salutation["english"]).toBe("hello");
+        expect(salutation.spanish || "not supported").toBe("not supported");
     });
-    
-    it("should know that can use && to protect from undefined access", function(){
-       var salutation = {
-           italian : {
-               always : "ciao",
-               morning : "buona mattina"
-           },
-           english : {
-               always : "hello",
-               morning : "good morning"
-           }
-       };
-       
-       expect(salutation.italian.always).toBe("ciao");
-       expect(salutation["english"]["morning"]).toBe("good morning");
-       expect((salutation.spanish && salutation.spanish.morning) || "not supported").toBe("not supported");
+
+    it("should know that can use && to protect from undefined access", function() {
+        var salutation = {
+            italian: {
+                always: "ciao",
+                morning: "buona mattina"
+            },
+            english: {
+                always: "hello",
+                morning: "good morning"
+            }
+        };
+
+        expect(salutation.italian.always).toBe("ciao");
+        expect(salutation["english"]["morning"]).toBe("good morning");
+        expect((salutation.spanish && salutation.spanish.morning) || "not supported").toBe("not supported");
     });
-    
+
     /* JOHN RESIG */
-    
-    it("should be able to write and understand the Function.prototype.bind of prototype.js", function(){
-        var salutation = function(prefix, suffix){
+
+    it("should be able to write and understand the Function.prototype.bind of prototype.js", function() {
+        var oldBind = Function.prototype.bind;
+        var salutation = function(prefix, suffix) {
             return prefix + " " + this.name + suffix;
         };
-        
+
         var emanuele = {
-            name : "Emanuele"
+            name: "Emanuele"
         };
-        
+
         var simone = {
-            name : "Simone"
+            name: "Simone"
         };
-        
-        Function.prototype.bind = function(){
+
+        Function.prototype.bind = function() {
             var fn = this, args = Array.prototype.slice.call(arguments), obj = args.shift();
-            return function(){
+            return function() {
                 return fn.apply(obj, Array.prototype.concat.call(args, Array.prototype.slice.call(arguments)));
             };
         };
-        
-        expect(salutation.bind(emanuele)("Hello","!!!")).toBe("Hello Emanuele!!!");
-        expect(salutation.bind(emanuele,"Hello","!!!")()).toBe("Hello Emanuele!!!");
-        expect(salutation.bind(emanuele,"Hello")("!!!")).toBe("Hello Emanuele!!!");
-        expect(salutation.bind(simone)("Hello","?")).toBe("Hello Simone?");        
-        
-        delete Function.prototype.bind;
-        
-        
+
+        expect(salutation.bind(emanuele)("Hello", "!!!")).toBe("Hello Emanuele!!!");
+        expect(salutation.bind(emanuele, "Hello", "!!!")()).toBe("Hello Emanuele!!!");
+        expect(salutation.bind(emanuele, "Hello")("!!!")).toBe("Hello Emanuele!!!");
+        expect(salutation.bind(simone)("Hello", "?")).toBe("Hello Simone?");
+
+        Function.prototype.bind = oldBind;
+
+
     });
-    
-    it("should be aware of variable and function hoisting", function(){
-        
+
+    it("should be aware of variable and function hoisting", function() {
+
         expect(typeof hoistedFunctionVar === 'undefined').toBe(true);
         expect(typeof hoistedFunction === 'function').toBe(true);
-        
-        function hoistedFunction(){};
-        var hoistedFunctionVar = function(){};
+
+        function hoistedFunction() {
+        }
+        ;
+        var hoistedFunctionVar = function() {
+        };
     });
-    
-    it("should know that even if you can go below the return you can use hoisted functions ", function(){
-        function happyFunction(){
+
+    it("should know that even if you can go below the return you can use hoisted functions ", function() {
+        function happyFunction() {
             return stealthFunction();
-            
+
             function stealthFunction() {
                 return "happy stealth";
-            };
-        };
+            }
+            ;
+        }
+        ;
         expect(happyFunction()).toBe("happy stealth");
     });
-    
-    it("should know that she can call a function by his name inside the function", function(){
-        function fact(n){
-            if ( n < 0) throw "Illegal argument";
-            if (n === 0) return 1;
-            return n * fact(n -1);
+
+    it("should know that she can call a function by his name inside the function", function() {
+        function fact(n) {
+            if (n < 0)
+                throw "Illegal argument";
+            if (n === 0)
+                return 1;
+            return n * fact(n - 1);
         }
-        
-        expect(fact(4)).toBe(4*3*2*1);
+
+        expect(fact(4)).toBe(4 * 3 * 2 * 1);
     });
-    
-    it("should know that you can name functions and assign them to variables, or do both", function(){
-        var myFirstName =  function myBuddyName(){
+
+    it("should know that you can name functions and assign them to variables, or do both", function() {
+        var myFirstName = function myBuddyName() {
             expect(myFirstName === myBuddyName).toBe(true);
         };
         expect(typeof myBuddyName).toBe('undefined');
     });
-    
-    it("should know that you can refer to function names even if they are method of an object", function(){
+
+    it("should know that you can refer to function names even if they are method of an object", function() {
         var math = {
-            fact : function myFact(n){
-                if ( n < 0) throw "Illegal argument";
-                if (n === 0) return 1;
-                return n * myFact(n -1);
+            fact: function myFact(n) {
+                if (n < 0)
+                    throw "Illegal argument";
+                if (n === 0)
+                    return 1;
+                return n * myFact(n - 1);
             },
-            fact2 : function (n){
-                if ( n < 0) throw "Illegal argument";
-                if (n === 0) return 1;
-                return n * math.fact2(n -1);
+            fact2: function(n) {
+                if (n < 0)
+                    throw "Illegal argument";
+                if (n === 0)
+                    return 1;
+                return n * math.fact2(n - 1);
             },
-            fact3 : function (n){
-                if ( n < 0) throw "Illegal argument";
-                if (n === 0) return 1;
-                return n * this.fact3(n -1);
+            fact3: function(n) {
+                if (n < 0)
+                    throw "Illegal argument";
+                if (n === 0)
+                    return 1;
+                return n * this.fact3(n - 1);
             }
         };
+
+        expect(math.fact(4)).toBe(4 * 3 * 2 * 1);
+        expect(math.fact2(4)).toBe(4 * 3 * 2 * 1);
+        expect(math.fact3(4)).toBe(4 * 3 * 2 * 1);
+    });
+
+    it("shoud be very carefull when he refer to objects that can be deleted", function() {
+        var simone = {
+            surname: "Rampichini",
+            name: "Simone",
+            getSurname: function() {
+                return simone.surname;
+            }
+        };
+
+        var emanuele = {
+            name: "Emanuele",
+            getSurname: simone.getSurname //thanks brother, i will borrow the surname from you :)
+        };
+
+        expect(simone.getSurname()).toBe("Rampichini");
+        expect(emanuele.getSurname()).toBe("Rampichini");
+
+        simone = null; //i don't think so... brother! Autodestruction!
+
+        try {
+            emanuele.getSurname();
+            expect(true).toBe(false);
+        } catch (e) {
+            expect(true).toBe(true);
+        }
+    });
+
+    it("should know that if a named function is named can always refer to self with the name", function() {
+        var math = {
+            anonFact: function(n) {
+                if (n < 0)
+                    throw "Illegal argument";
+                if (n === 0)
+                    return 1;
+                return n * math.anonFact(n - 1);
+            },
+            namedFact: function namedFact(n) {
+                if (n < 0)
+                    throw "Illegal argument";
+                if (n === 0)
+                    return 1;
+                return n * namedFact(n - 1);
+            }
+        };
+
+        expect(math.anonFact(4)).toBe(1 * 2 * 3 * 4);
+        expect(math.namedFact(4)).toBe(1 * 2 * 3 * 4);
+
+        var anonFact = math.anonFact;
+        var namedFact = math.namedFact;
+
+        math = null;
+
+        try {
+            anonFact(4);
+            expect(true).toBe(false);
+        } catch (e) {
+            expect(true).toBe(true);
+        }
+        expect(namedFact(4)).toBe(1 * 2 * 3 * 4);
+    });
+
+    it("should know that a recursive function can refer to itself via rguments.callee", function() {
+        var math = {
+            fact: function(n) {
+                if (n < 0)
+                    throw "Illegal argument";
+                if (n === 0)
+                    return 1;
+                return n * arguments.callee(n - 1);
+            }
+        };
+
+        expect(math.fact(4)).toBe(1 * 2 * 3 * 4);
+        var fact = math.fact;
+        math = null;
+        expect(fact(4)).toBe(1 * 2 * 3 * 4);
+    });
+
+    it("should know that function and objects can have properties", function() {
+        var fn = function pippo() {
+        };
+        var obj = {};
+        fn.customProp = "blabla";
+        obj.customProp = "blabla";
+
+        expect(fn.customProp).toBe("blabla");
+        expect(obj.customProp).toBe("blabla");
+    });
+
+    it("shoud be able to cache a function", function() {
+        function fact(n) {
+            if (n < 0)
+                throw "Illegal argument";
+            if (n === 0)
+                return 1;
+            if (fact.cache[n]){
+                return fact.cache[n];
+            } else {
+                fact.cache[n] = n * fact(n - 1);
+                return fact.cache[n];
+            }
+        }
+        fact.cache  = {};
         
-        expect(math.fact(4)).toBe(4*3*2*1);
-        expect(math.fact2(4)).toBe(4*3*2*1);
-        expect(math.fact3(4)).toBe(4*3*2*1);
+        expect(fact(5)).toBe(120);
+        expect(fact.cache[5]).toBe(120);
+        expect(fact.cache[4]).toBe(24);
+        expect(fact.cache[3]).toBe(6);
+        expect(fact.cache[2]).toBe(2);
+        
     });
 });
